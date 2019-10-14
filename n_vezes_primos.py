abd62055d74379dbd9d9c0085d1519395cb53bed
kevinpbela <55717326+kevinpbela@users.noreply.github.com>
2019-10-14 20:14:23 -0300

    Update n_vezes_primos.py

--- n_vezes_primos.py
+++ n_vezes_primos.py
@@ -2,23 +2,26 @@ import os from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-n = 100
-n2 = 0
-divisores = 0
-nvezes = 0
+@app.route('/')
+def nao_entre_em_panico():
+  if request.headers.get('Authorization') == '42':
+  n = 100
+  n2 = 0
+  divisores = 0
+  nvezes = 0
 
-while nvezes < n:
-  n2 += 1
-  for divisor in range (1, n2+1):
-    if n2 % divisor == 0:   # se n � divisivel por divisor
-      divisores += 1   #em caso verdadeiro, incrementa 1 na variav�l divisores
-  if divisores == 2:
-    return(n2)
-    nvezes += 1
-    divisores = 0
-  else:
-    divisores = 0 
- 
+   while nvezes < n:
+     n2 += 1
+     for divisor in range (1, n2+1):
+       if n2 % divisor == 0:   # se n � divisivel por divisor
+         divisores += 1   #em caso verdadeiro, incrementa 1 na variav�l divisores
+     if divisores == 2:
+       return(n2)
+       nvezes += 1
+       divisores = 0
+     else:
+       divisores = 0 
+   return jsonify({"message": "Não entre em pânico!"})
 
 if __name__ == "__main__":
 port = int(os.environ.get("PORT", 5000))
