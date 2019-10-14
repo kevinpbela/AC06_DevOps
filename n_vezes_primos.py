0a7bc23d6c68c3d464e58ae0367d78cb4de30a74
kevinpbela <55717326+kevinpbela@users.noreply.github.com>
2019-10-14 20:03:40 -0300

    Add files via upload

---
+++ n_vezes_primos.py
@@ -0,0 +1,25 @@
+import os from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+n = 100
+n2 = 0
+divisores = 0
+nvezes = 0
+
+while nvezes < n:
+  n2 += 1
+  for divisor in range (1, n2+1):
+    if n2 % divisor == 0:   # se n � divisivel por divisor
+      divisores += 1   #em caso verdadeiro, incrementa 1 na variav�l divisores
+  if divisores == 2:
+    return(n2)
+    nvezes += 1
+    divisores = 0
+  else:
+    divisores = 0 
+ 
+
+if __name__ == "__main__":
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
